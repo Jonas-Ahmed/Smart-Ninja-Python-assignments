7019bc2264ba29653a39c2452c9220810f76b444
Jonas-Ahmed <87078211+Jonas-Ahmed@users.noreply.github.com>
2021-07-07 18:47:02 +0200

    Delete Guess a number.py

--- Guess a number.py
+++
@@ -1,8 +0,0 @@
-secret = 9
-guess = int(input("Please guess a number between 1-10\n"))
-                
-if guess != secret:
-    print(f"Sorry wrong number, the correct one was {secret}")
-else:
-    print(f"you are correct the secret number is {secret}")
-
